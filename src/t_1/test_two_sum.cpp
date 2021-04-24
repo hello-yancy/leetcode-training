8b79d2d6d5f37dff1ffb15c4bcab366c44ae0a4c
hello-yancy <yujd2000@sina.com>
2021-04-24 13:37:53 +0800

    mod test frame

--- src/t_1/test_two_sum.cpp
+++ src/t_1/test_two_sum.cpp
@@ -13,11 +13,12 @@ public:
 
 protected:
     std::shared_ptr<TwoSum> m_ptr;
-}
+};
 
 TEST_F(TwoSumTest, test_1)
 {
-    std::vector<int> rst = m_ptr->twoSum({2, 7, 11, 15}, 9);
+    std::vector<int> vec = {2, 7, 11, 15};
+    std::vector<int> rst = m_ptr->twoSum(vec, 9);
     ASSERT_EQ(2, rst.size());
     EXPECT_EQ(0, rst[0]);
     EXPECT_EQ(1, rst[1]);
